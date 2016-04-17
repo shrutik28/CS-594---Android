d77ca39a8799f0a95f88c0aeeea0e0a2475f8e81
shrutik28 <kapadiya.shruti@yahoo.com>
2016-04-16 23:10:50 -0700

    Update Constants.java

--- MyFlickr/app/src/main/java/com/example/shruti/myflickr/Constants.java
+++ MyFlickr/app/src/main/java/com/example/shruti/myflickr/Constants.java
@@ -1,8 +1,6 @@
 package com.example.shruti.myflickr;
 
-/**
- * Created by mark on 4/29/15.
- */
+
 public class Constants {
     public static final int NUM_PHOTOS = 10;
     public static final String TAG = "my code";
